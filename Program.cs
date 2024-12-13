b9819b712f32c3d485d9d98fdfe4a7fca75db680
falau <falau@LENOVO>
2024-12-13 16:19:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Set2Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dimensiune vector: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Elemente vector: ");
+            int[] v = new int[n];
+
+            for(int i = 0; i < n; i++)
+            {
+                v[i] = int.Parse(Console.ReadLine());
+            }
+            bool crescator = true;
+                
+            for(int i = 0;i < v.Length - 1; i++)
+            {
+                if (v[i] > v[i+1])
+                {
+                    crescator = false;
+                    break;
+                }
+            }
+            Console.WriteLine($" este crescator? {crescator}");
+        }
+    }
+}
